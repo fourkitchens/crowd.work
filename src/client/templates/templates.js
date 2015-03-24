82928a599cd9f90ad1e4f464396a6ed303d767c9
Patrick Coffey <patrickcoffey48@gmail.com>
2015-03-23 19:26:33 -0500

    Add a main toolbar

---
+++ src/client/templates/templates.js
@@ -0,0 +1,7 @@
+Meteor.startup(function() {
+  var body = document.getElementsByTagName('body')[0];
+  body.setAttribute('fullbleed', '');
+  body.setAttribute('vertical', '');
+  body.setAttribute('layout', '');
+  body.setAttribute('unresolved', '');
+});
